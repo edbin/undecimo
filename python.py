c1c3432cce42d6074dc970f1ecd2c292362704ae
edbin <73134283+edbin@users.noreply.github.com>
2024-07-28 22:24:32 -0600

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,6 @@
 # Realice los siguientes ejercicios usando el comando print:
 #1. Muestre por pantalla la fecha actual
-#2. 
+#2. Muestre por pantalla lo siguiente: "Factor del Dolar"
+#3. Guarde en una variable el factor del dolar actual
+#4. En otra variable convierta 1,500 dolares a lempiras (usando la variable del factor dolar) 
 print("Nos vemos el jueves proximo!")
