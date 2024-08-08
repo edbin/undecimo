0de8621e92af8c9f2bb8f08ca65bbfd310f2871c
edbin <73134283+edbin@users.noreply.github.com>
2024-08-08 09:58:10 -0600

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,10 @@
-# Realice los siguientes ejercicios usando el comando print:
-#1. Muestre por pantalla la fecha actual
-#2. Muestre por pantalla lo siguiente: "Factor del Dolar"
-#3. Guarde en una variable el factor del dolar actual
-#4. En otra variable convierta 1,500 dolares a lempiras (usando la variable del factor dolar) 
-print("Nos vemos el jueves proximo!")
+# Realice los siguientes ejercicios de repaso:
+# declare 4 variables numericas (num1, num2, num3 y num4)
+# solicite al usuario que ingrese el valor a cada variable (input)  
+# las primeras dos variables se suman, la tercera se multiplica y la ultima se divide
+# el resultado de la operacion matematica la guarda en una variable (respuesta) 
+# muestre por pantalla: "El resultado de la operacion es: " 
+
+print("cualquier duda me consulta")
+  
+
