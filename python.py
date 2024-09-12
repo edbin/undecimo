8fa967fb0a8e3310c2a0566d7bf6b61e72f232e1
edbin <73134283+edbin@users.noreply.github.com>
2024-09-12 11:26:22 -0600

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,19 @@
-# Realice los siguientes ejercicios de repaso:
-# declare 4 variables numericas (num1, num2, num3 y num4)
-# solicite al usuario que ingrese el valor a cada variable (input)  
-# las primeras dos variables se suman, la tercera se multiplica y la ultima se divide
-# el resultado de la operacion matematica la guarda en una variable (respuesta) 
-# muestre por pantalla: "El resultado de la operacion es: " 
+import random
 
-print("cualquier duda me consulta")
+colores = ["rojo", "naranja", "amarillo"]
+
+print("El color es:", colores[2])
+
+seleccion = input("Escoja un numero: ")
+
+computadora = random.randint(0,5)
+
+if int(seleccion) == computadora:
+  print("seleccionaron el mismo numero")
+else:
+  print("Seleccionaron diferentes numeros")
+
+print("Opcion de la computadora ", computadora)
+
   
 
