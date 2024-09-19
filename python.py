ae92e88fb6396e11a6c581375cc469a871001991
edbin <73134283+edbin@users.noreply.github.com>
2024-09-19 13:06:06 -0600

    Update python.py

--- python.py
+++ python.py
@@ -4,6 +4,7 @@ dato1 = int(input("Ingrese el primer valor "))
 dato2 = int(input("Ingrese el segudo valor "))
 
 opcion = input("1 - Suma, 2- Resta, 3- Multiplica, 4- Divide: ")
+resultado = 0
 
 if opcion == "1":
     print("Sera una suma")
