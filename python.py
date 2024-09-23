69229437a5cce8f6618c2ad347e333314e04d866
edbin <73134283+edbin@users.noreply.github.com>
2024-09-23 13:17:36 -0600

    Update python.py

--- python.py
+++ python.py
@@ -1,27 +1,7 @@
-# CALCULARADORA
+#EJERCICIO EN PYTHON
 
-dato1 = int(input("Ingrese el primer valor "))
-dato2 = int(input("Ingrese el segudo valor "))
+print("Ejercicio de 5 puntos")
 
-opcion = input("1 - Suma, 2- Resta, 3- Multiplica, 4- Divide: ")
-resultado = 0
-
-if opcion == "1":
-    print("Sera una suma")
-    resultado = dato1 + dato2
-    print(resultado)
-elif opcion == "2": 
-    print("Sera una resta")
-    resultado = dato1 - dato2
-    print(resultado)    
-elif opcion == "3":
-    print("Sera una multicplicacion")
-    resultado = dato1 * dato2
-    print(resultado)    
-elif opcion == "4":
-    print("Sera una division")
-    resultado = dato1 / dato2
-    print(resultado)
-else:
-    print("Opcion incorrecta!")
+#Genere un programa donde el usuario ingrese la edad, luego usted determine si es mayor de edad o menor de edad.
+# 7es mayor si tiene mas 1 años
         
