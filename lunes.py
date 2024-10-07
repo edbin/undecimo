9f86331a13b635927e762ded81d7fa71722d18f5
edbin <73134283+edbin@users.noreply.github.com>
2024-10-07 06:35:29 -0600

    Create lunes.py

---
+++ lunes.py
@@ -0,0 +1,8 @@
+Ejercicios en python:
+Ingrese una lista en python con sus 3 clases favoritas
+Muestre por pantalla la primer clase y luego la ultima
+Ciclo for, realice el siguiente ejercicio:
+--------ONE COMPILER----------
+nums = [4, 78, 9, 84]
+for n in nums:
+    print(n)
