d62b080fb85992e6da060b5bddae3b4c43395726
edbin <73134283+edbin@users.noreply.github.com>
2024-10-07 06:29:54 -0600

    Delete python.py

--- python.py
+++
@@ -1,7 +0,0 @@
-#EJERCICIO EN PYTHON
-
-print("Ejercicio de 5 puntos")
-
-#Genere un programa donde el usuario ingrese la edad, luego usted determine si es mayor de edad o menor de edad.
-# 7es mayor si tiene mas 1 años
-        
