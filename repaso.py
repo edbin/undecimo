81c2c110f4402a0fe074ee756c437c8f2edebbf3
edbin <73134283+edbin@users.noreply.github.com>
2024-10-21 11:36:13 -0600

    Create repaso.py

---
+++ repaso.py
@@ -0,0 +1,5 @@
+Desarrolle los siguientes ejercicios:
+a.	Cree una lista 5 paises de latinoamerica y los muestra por pantalla (print) 
+b.	Usando el ciclo for muestre el correlativo de números del 100 al 150
+c.	Defina una función (def) para sumar dos números (mediante dos variables: num1 y num2) y muestre el resultado 
+d.	Usando el comando random (import random) que la computadora le muestre un valor aleatorio entre 0 a 10 
