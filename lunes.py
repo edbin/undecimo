a9a855077f4bd2e9b4136f422d5f7e5dedbda382
edbin <73134283+edbin@users.noreply.github.com>
2024-10-21 11:35:35 -0600

    Update lunes.py

--- lunes.py
+++ lunes.py
@@ -1,8 +1,5 @@
-Ejercicios en python:
-Ingrese una lista en python con sus 3 clases favoritas
-Muestre por pantalla la primer clase y luego la ultima
-Ciclo for, realice el siguiente ejercicio:
---------ONE COMPILER----------
-nums = [4, 78, 9, 84]
-for n in nums:
-    print(n)
+Desarrolle los siguientes ejercicios:
+a.	Cree una lista 5 paises de latinoamerica y los muestra por pantalla (print) 
+b.	Usando el ciclo for muestre el correlativo de números del 100 al 150
+c.	Defina una función (def) para sumar dos números (mediante dos variables: num1 y num2) y muestre el resultado 
+d.	Usando el comando random (import random) que la computadora le muestre un valor aleatorio entre 0 a 10 
